31c80f200bac540b249017a59bf82909fe44a2b5
takonomura <takonomura@users.noreply.github.com>
2018-01-30 16:16:36 +0900

    Enable debug option for Poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,9 @@ WebMock.disable_net_connect!(allow_localhost: true)
 
 require 'capybara/rspec'
 require 'capybara/poltergeist'
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, debug: true)
+end
 Capybara.javascript_driver = :poltergeist
 Capybara.default_max_wait_time = 5
 
